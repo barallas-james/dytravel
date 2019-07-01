aafd02322777c1a55cd809345e2fc8b6f27bec77
james <barallas.james@gmail.com>
2019-07-01 17:00:05 +0800

    Update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,8 @@
+// When the page isn't loaded yet, preloader shows
 $(window).on('load',function() {
     $(".preloader").fadeOut(1000);
 })
+// When the page is ready, the preloader will fade and the page will show
+$(document).ready(function() {
+
+});
